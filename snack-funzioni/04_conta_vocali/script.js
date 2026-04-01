5f0a930669dad759e04fe16ea5ebafda64a9d490
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-01 16:59:03 +0200

    ADD snack 4 simple function TODO arrow

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,9 +4,24 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vocalizzatore(parola) {
+    const scomponiParola = parola.split('');
+    const vocali = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    const nuovaParola = [];
+    let nuovissimaParola;
+    
+    for (let i = 0; i < scomponiParola.length; i++) {
+        if (vocali.includes(scomponiParola[i])) {
+            nuovaParola.push(scomponiParola[i]);
+        }
+    }
+    nuovissimaParola = nuovaParola.join('');
+    return nuovissimaParola;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+let parolaVocali =vocalizzatore(word);
+console.log(parolaVocali);
 
 
 
