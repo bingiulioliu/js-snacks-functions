c96f9ca041bf24951a75f2f062cc0a45737dc596
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-01 17:51:55 +0200

    ADD snack 5 arrow

--- snack-funzioni/05_bonus/script.js
+++ snack-funzioni/05_bonus/script.js
@@ -10,7 +10,17 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 function saluti(nome) {
-    let currentHour = new Date().getHours();
+    const currentHour = new Date().getHours();
+    if (currentHour <= 13){
+        return console.log(`Buongiorno ${nome}`);
+    } else if (currentHour <=17){
+        return console.log(`Buon pomeriggio ${nome}`);
+    } else
+        return console.log(`Buonasera ${nome}`);
+}
+// arrow
+const salutiArrow = nome => {
+    const currentHour = new Date().getHours();
     if (currentHour <= 13){
         return console.log(`Buongiorno ${nome}`);
     } else if (currentHour <=17){
@@ -19,12 +29,20 @@ function saluti(nome) {
         return console.log(`Buonasera ${nome}`);
 }
 
+// arrow ternario
+const salutiTernari = nome => {
+    const currentHour = new Date().getHours();
+return  currentHour <= 13   ?   `Buongiorno ${nome}`:
+        currentHour <= 17   ?   `Buon pomeriggio ${nome}`:
+                            `Buonasera ${nome}`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 saluti(name);
 
+salutiArrow(name);
 
-
+console.log(salutiTernari(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
