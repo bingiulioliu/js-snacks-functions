779a2378878133dd995aaa5098043f65e7923f6d
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-01 17:07:32 +0200

    ADD snack 4 arrow

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -1,10 +1,26 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
+const word2 = 'aiuole';
 
 
 // Dichiara la funzione qui.
 function vocalizzatore(parola) {
+    const scomponiParola = parola.split('');
+    const vocali = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    const nuovaParola = [];
+    let nuovissimaParola;
+    
+    for (let i = 0; i < scomponiParola.length; i++) {
+        if (vocali.includes(scomponiParola[i])) {
+            nuovaParola.push(scomponiParola[i]);
+        }
+    }
+    nuovissimaParola = nuovaParola.join('');
+    return nuovissimaParola;
+}
+// arrow
+const vocalizzatoreArrow = (parola) => {
     const scomponiParola = parola.split('');
     const vocali = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     const nuovaParola = [];
@@ -23,6 +39,7 @@ function vocalizzatore(parola) {
 let parolaVocali =vocalizzatore(word);
 console.log(parolaVocali);
 
-
+let parolaVocaliArrow =vocalizzatoreArrow(word2);
+console.log(parolaVocaliArrow);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
